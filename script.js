f5ecbef87a5276c1323a42318090c7bc59083192
NeBala5078 <149180341+NeBala5078@users.noreply.github.com>
2024-01-19 11:56:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-//initial state
-document.querySelector('.js-active-player').innerHTML = null;
-let players = { 0 : 0, 1 : 0, 2 : 0, 3:0 };
-let score = { 0 : 0, 1 : 0, 2 : 0, 3:0 };
-let playerCounter = 0;
-let playersInGame = 0;
-let lastScore = 0;
-let undoable = false;
+newGame();
 
 function addPlayer () {
-    if (playerCounter < 4) {
-    players[playerCounter] = document.querySelector('#player').value;
-    score[playerCounter] = 0;
-    playersInGame = playerCounter;
-    playerCounter++;
-    document.querySelector('#player').value = null;
-    renderPlayerTemplate();}
+    if (players.length < 4) {
+        players.push(document.querySelector('#player').value);
+        score.push(0);
+        document.querySelector('#player').value = null;
+        renderPlayerTemplate();
+        }
     else {
        alert('Maximum 4 játékos játszhatja a játékot'); 
     }
@@ -22,7 +14,7 @@ function addPlayer () {
 
 function renderPlayerTemplate () {
     let playerTemplate = '';
-    for(let i = 0; i<=playersInGame; i++) {
+    for(let i = 0; i<players.length; i++) {
         playerTemplate += `
         <div class="player player${i+1} js-player${i+1}">
             <h3>${players[i]}</h3>
@@ -35,7 +27,6 @@ function renderPlayerTemplate () {
 
 function start() {
     document.querySelector('.js-setup').style.visibility = "hidden";
-    playerCounter = 0;
     renderActivePlayer();
     document.querySelector('.add-score-container').style.visibility= "visible";
     document.querySelector('#player').value = null;
@@ -43,53 +34,48 @@ function start() {
 
 function renderActivePlayer() {
     document.querySelector('.js-active-player').innerHTML = `
-    <div class="active-container player${playerCounter+1}">
-    <h2>${players[playerCounter]} köre</h2>
+    <div class="active-container player${nextPlayerIndex+1}">
+    <h2>${players[nextPlayerIndex]} köre</h2>
     </div>
     `;
 }
 
 function addScore() {
     lastScore = Number(document.querySelector('.js-new-score').value);
-    score[playerCounter] += lastScore;
+    score[nextPlayerIndex] += lastScore;
     renderPlayerTemplate();
     document.querySelector('.js-new-score').value = 0;
-    playerCounter < playersInGame ? playerCounter++ : playerCounter=0;
+    nextPlayerIndex < players.length-1 ? nextPlayerIndex++ : nextPlayerIndex=0;
     renderActivePlayer();
-    return lastScore, undoable = true;
+    undoable = true;
     }
 
 
 function gameOver() {
-    let winner = '';
-    let maxScore = 0;
-    for (let i = 0; i<= playersInGame; i++) {
-        if ( score[i] > maxScore) {
-            maxScore = score[i];
-            winner = players[i];
+    let winner = [];
+    let maxScore = Math.max(...score);
+    
+    for (let i = 0; i<= players.length - 1; i++) {
+        if ( score[i] === maxScore) {
+            winner.push( players[i] );
         }
     }
-    //check for multiple winners
-    let multipleWinners = [];
-    for (let i = 0; i <= playersInGame; i++) {
-        if (score[i] == maxScore && players[i] != winner) {  
-            multipleWinners.push(players[i]); 
-    }}
-    if (multipleWinners.length == 0) {
+        
+    if (winner.length === 1) {
     alert('Gratulálunk ' + winner + ' Nyertél!!');
     } else {
-        alert('Döntetlen! A nyertesek: ' + winner + ' ' + multipleWinners)
+        alert('Döntetlen! A nyertesek: ' + winner );
     }
 }
 
 
 function undo() {
     if (undoable) {
-    playerCounter != 0 ? playerCounter--  : playerCounter = playersInGame;
-    score[playerCounter] -= lastScore;
+    nextPlayerIndex != 0 ? nextPlayerIndex--  : nextPlayerIndex = players.length - 1;
+    score[nextPlayerIndex] -= lastScore;
     renderPlayerTemplate();
     renderActivePlayer();
-    return undoable = false;}
+    undoable = false;}
     else {
         alert ('Csak a legutóbb beírt pont vonható vissza');
     }
@@ -97,10 +83,9 @@ function undo() {
 
 function newGame() {
 document.querySelector('.js-active-player').innerHTML = null;
-players = { 1 : 0, 2 : 0, 3 : 0, 4:0 };
-score = { 1 : 0, 2 : 0, 3 : 0, 4:0 };
-playerCounter = 0;
-playersInGame = 0;
+players = [];
+score = [];
+nextPlayerIndex = 0;
 document.querySelector('.js-setup').style.visibility = 'visible';
 document.querySelector('.js-score').innerHTML = null;
 document.querySelector('.add-score-container').style.visibility= "hidden"
